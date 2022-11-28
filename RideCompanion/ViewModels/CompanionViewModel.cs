bfb54f3e1d0177671cbcbcb5a71ada2bc495f9ad
Artem Konkin <temakonkin@gmail.com>
2022-11-28 17:47:24 +0600

    cqrs

---
+++ RideCompanion/ViewModels/CompanionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Companion.Domain.Dto;
+
+namespace RideCompanion.ViewModels;
+
+/// <summary>
+/// Trip
+/// </summary>
+public class CompanionViewModel
+{
+    public CompanionDto CompanionDto { get; set; }
+}
